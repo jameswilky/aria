d8d175ae40c8d9f0beecf6284171a11272b36214
James Wilkinson <videv9887@gmail.com>
2023-09-20 13:04:41 +0000

    added tests setup

---
+++ tests/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e  # Exit on any error
+
+# Get Working directory and Script name
+WD=$(cd "$(dirname "$0")" && pwd)
+SCRIPT="$(readlink -f "$0")"
+
+cd $WD
+
+echo "===== Executing script $SCRIPT from $PWD ===== "
+# Print each command before using it
+set -x
+
+###############################################################################################
+
+pnpm exec playwright install
+pnpm exec playwright install-deps
