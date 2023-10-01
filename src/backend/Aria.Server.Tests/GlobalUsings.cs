0c435051a4ca6771ae6c03cfcfc2cb2ce94906f9
James Wilkinson <videv9887@gmail.com>
2023-10-01 08:39:03 +0000

    refactored startup and added nunit

--- src/backend/Aria.Server.Tests/GlobalUsings.cs
+++ src/backend/Aria.Server.Tests/GlobalUsings.cs
@@ -1,1 +1,6 @@
+global using Microsoft.AspNetCore.Hosting;
+global using Microsoft.AspNetCore.TestHost;
 global using NUnit.Framework;
+global using System.Net.Http;
+global using Aria.Server;
+global using Microsoft.AspNetCore.Mvc.Testing;
--- src/backend/Aria.Server/Program.cs
+++ src/backend/Aria.Server/Program.cs
@@ -1,96 +1,16 @@
-using NSwag;
-using NSwag.Generation.Processors.Security;
-using Aria.Database.Entities;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using Aria.Server.Services.UserService;
+using Aria.Server;
 
 internal class Program
 {
-
-    private static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
-        var app = Build(builder);
-        UseMiddleware(app);
-        CreateDbScope(app);
-        app.Run();
+        CreateHostBuilder(args).Build().Run();
     }
 
-    private static void CreateDbScope(WebApplication app)
+    public static IHostBuilder CreateHostBuilder(string[] args)
     {
-        using (var scope = app.Services.CreateScope())
-        {
-            var services = scope.ServiceProvider;
-            var context = services.GetRequiredService<AriaContext>();
-            context.Database.EnsureCreated();
-        }
-    }
-
-    private static void UseMiddleware(WebApplication app)
-    {
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-        }
-
-        app.UseAuthentication();
-        app.UseAuthorization();
-        app.UseOpenApi();
-        app.UseSwaggerUi3();
-        app.MapControllers();
-    }
-    private static WebApplication Build(WebApplicationBuilder builder)
-    {
-        // Add API Explorer to enable NSwag
-        builder.Services.AddEndpointsApiExplorer();
-
-        // Add Services
-        builder.Services.AddDbContext<AriaContext>();
-        builder.Services.AddTransient<UserService>();
-
-        // Register NSwag services before building the app
-        builder.Services.AddOpenApiDocument(document =>
-        {
-            document.Title = "Aria";
-            document.AddSecurity("JWT", Enumerable.Empty<string>(), new OpenApiSecurityScheme
-            {
-                Type = OpenApiSecuritySchemeType.ApiKey,
-                Name = "Authorization",
-                In = OpenApiSecurityApiKeyLocation.Header,
-                Description = "Copy 'Bearer ' + valid JWT token into field"
-            });
-            document.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor("JWT"));
-        });
-
-        // Setup auth
-        var jwt = new JwtSettings();
-        builder.Configuration.Bind(nameof(JwtSettings), jwt);
-        builder.Services.AddSingleton(jwt);
-        builder.Services.AddAuthentication(options =>
-        {
-            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        }).AddJwtBearer(options =>
-        {
-            options.RequireHttpsMetadata = false;
-            options.TokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateIssuer = true,
-                ValidIssuer = jwt.Issuer,
-                ValidateAudience = true,
-                ValidAudience = jwt.Audience,
-                ValidateLifetime = true,
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwt.Key)),
-                ValidateIssuerSigningKey = true,
-            };
-        });
-
-        builder.Services.AddAuthorization();
-        builder.Services.AddControllers();
-
-        return builder.Build();
+        return Host.CreateDefaultBuilder(args)
+              .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 
 }
