606e5f8d6bd34bbf5566cd41253ce4974ca42f01
James Wilkinson <videv9887@gmail.com>
2023-10-15 01:25:17 +0000

    Added first test casxe

--- src/dynamock/src/client/dynamockClient.ts
+++ src/dynamock/src/client/dynamockClient.ts
@@ -1,19 +1,48 @@
-import fetch from 'node-fetch';
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 /**
  * This interface is used to configure the dynamock client
  */
 interface DynamockClientConfig {
-    /**
-     * Used to point to the currently running dynamock server
-     */
-    host: string
+  /**
+   * Used to point to the currently running dynamock server
+   */
+  host: string;
 }
 
-export default function dynamockClient (config: DynamockClientConfig ){
-    const clientId = uuidv4()
+type HttpMethod =
+  | "GET"
+  | "POST"
+  | "PUT"
+  | "DELETE"
+  | "PATCH"
+  | "HEAD"
+  | "OPTIONS"
+  | "CONNECT"
+  | "TRACE";
 
-    return {
+export interface Locator {
+  url: string;
+  method: HttpMethod;
+  filter?: RequestFilter;
+}
 
-    }
+export interface Proxy {
+  headers?: Record<string, string>;
+  body?: object;
+  status: number;
+}
+
+export interface RequestFilter {
+  headers?: Record<string, string>;
+  body?: string;
+}
+
+export function dynamockClient(config: DynamockClientConfig) {
+  const clientId = uuidv4();
+
+  const intercept = async (locator: Locator, proxy: Proxy) => {};
+
+  return {
+    intercept,
+  };
 }
