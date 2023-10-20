0ba780129223b72cb8b32ad5a00846803fe83ec5
James Wilkinson <videv9887@gmail.com>
2023-10-20 14:25:12 +1100

    removed dynamock

--- src/client/tests/playwright/signup.spec.ts
+++ src/client/tests/playwright/signup.spec.ts
@@ -1,20 +1,20 @@
 import { test, expect } from '@playwright/test';
 
 test('Sign up new user', async ({ page }) => {
+	const user = 'john';
+
 	// Act
 	await page.goto('/');
 	await page.getByRole('link', { name: 'Signup' }).click();
 	await page.getByLabel('Username').click();
-	await page.getByLabel('Username').fill('john');
+	await page.getByLabel('Username').fill(`${user}`);
 	await page.getByLabel('Email').click();
-	await page.getByLabel('Email').fill('john@gmail.com');
+	await page.getByLabel('Email').fill(`${user}@gmail.com`);
 	await page.getByLabel('Password').click();
-	await page.getByLabel('Password').fill('john1234');
+	await page.getByLabel('Password').fill(`${user}1234`);
 	await page.getByRole('button', { name: 'Continue' }).click();
 
 	// Assert
 	await expect(page).toHaveURL('/dashboard');
-	await expect(page.getByText('Hello, john'));
-
+	await expect(page.getByText(`Hello, ${user}`)).toBeVisible();
 });
-
--- src/dynamock/README.md
+++
@@ -1,3 +0,0 @@
-# dynamock
-
-A simple mock server for creating dynamically generated routes and mock responses specifically for UI testing hybrid SSR frameworks like SvelteKit and NextJS while using additional server-side API calls.
