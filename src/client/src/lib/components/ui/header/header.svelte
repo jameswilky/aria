ef24f775e3cb7629d39334bc8259555c8cb4ccf4
James Wilkinson <videv9887@gmail.com>
2023-11-07 11:14:36 +0000

    Updated sidebar

--- src/client/src/lib/components/ui/header/header.svelte
+++ src/client/src/lib/components/ui/header/header.svelte
@@ -4,14 +4,10 @@
 	export let profile: Profile;
 </script>
 
-<nav class="fixed top-0 z-50 w-full bg-secondary border-b border-slate-700">
+<nav class="fixed top-0 z-50 w-full bg-primary-foreground border-b border-secondary">
 	<div class="px-3 py-3 lg:px-5 lg:pl-3">
 		<div class="flex items-center justify-between">
 			<div class="flex items-center justify-start">
-				<img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="FlowBite Logo" />
-				<span
-					class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white"
-				/>
 				<h1 class="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-3xl">Aria</h1>
 			</div>
 			<div class="flex items-center">
--- src/client/src/lib/components/ui/sidebar/sidebar.svelte
+++ src/client/src/lib/components/ui/sidebar/sidebar.svelte
@@ -1,6 +1,32 @@
 <script lang="ts">
+	import Button from '../button/button.svelte';
+	import { Home, Settings, MessageSquare, Text, MessageSquarePlus } from 'lucide-svelte';
 </script>
 
-<aside class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 bg-secondary" aria-label="Sidebar">
-	<div class="h-full px-3 pb-4 overflow-y-auto bg-red" />
+<aside class="h-screen flex flex-col w-64 fixedborder-r pt-16 border-secondary border-r-2">
+	<div
+		class="p-4 border-b border-slate-200 dark:border-slate-700 hover:primary dark:hover:bg-primary-foreground"
+	>
+		<a class="flex items-center space-x-3 text-primary dark:primary text-m" href="#">
+			<MessageSquarePlus />
+			<span>New Conversation</span>
+		</a>
+	</div>
+
+	<nav class="flex-grow overflow-y-auto p-4 border-b border-secondary">
+		<ul class="space-y-2">
+			<li class="rounded-md hover:primary dark:hover:bg-primary-foreground">
+				<a class="flex items-center space-x-3 text-primary dark:primary text-sm" href="#">
+					<Text />
+					<span>Conversation 1</span>
+				</a>
+			</li>
+		</ul>
+	</nav>
+	<div class="p-4 border-t border-slate-200 dark:border-slate-700">
+		<a class="flex items-center space-x-3 text-m font-medium" href="#">
+			<Settings />
+			<span>Settings</span>
+		</a>
+	</div>
 </aside>
